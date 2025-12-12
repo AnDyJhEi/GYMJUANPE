e63e2bd99b7bedea1529b133be646c35a1acf266
AnDyJhEi <andyordonez416@gmail.com>
2025-12-12 14:30:41 -0500

    mejoras

--- src/main/java/com/gymjuanpe_app/controller/HomeController.java
+++ src/main/java/com/gymjuanpe_app/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.gymjuanpe_app.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HomeController {
 
-  @GetMapping("/")
-  public String index() {
-    return "index";
-  }
+    @GetMapping("/api")
+    public String api() {
+        return "API Gym Juanpe funcionando OK";
+    }
 }
